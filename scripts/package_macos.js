6b9f807f51ea379c5320b4c44e0529399dcb1ff1
Aigars Dzerviniks <aigarsdz@protonmail.com>
2016-09-24 17:27:58 +0300

    Adds code signing step in the MacOS package script

--- scripts/package_macos.js
+++ scripts/package_macos.js
@@ -2,6 +2,7 @@ const packager    = require('electron-packager');
 const appdmg      = require('appdmg');
 const rimraf      = require('rimraf');
 const path        = require('path');
+const exec        = require('child_process').exec;
 const packageInfo = require(path.join(__dirname, '..', 'package.json'));
 
 if (process.platform === 'darwin') {
@@ -23,33 +24,44 @@ if (process.platform === 'darwin') {
         console.log('The following app bundles were created:\n\n');
         appPaths.forEach((path) => console.log(`\t${path}`));
         console.log();
+
+        if (process.env.CODE_SIGNING_AUTHORITY) {
+          appPaths.forEach((p) => {
+            exec("codesign --deep --force --verbose --sign '" +
+              process.env.CODE_SIGNING_AUTHORITY + "' " + p + "/Olden.app", (err, stdout, stderr) => {
+                if (err) {
+                  console.log(stderr);
+                }
+
+                dmgCreator = appdmg({
+                  source: path.join(__dirname, '..', 'appdmg.json'),
+                  target: path.join(__dirname, '..', 'dist', `Olden-${packageInfo.version}.dmg`)
+                });
+
+                dmgCreator.on('progress', (info) => {
+                  if (info.type === 'step-begin') {
+                    const line = `[ ${info.current}/${info.total} ] ${info.title}...`;
+
+                    process.stdout.write(line + ' '.repeat(45 - line.length));
+                  }
+
+                  if (info.type === 'step-end') {
+                    process.stdout.write(`[ ${info.status} ]\n`);
+                  }
+                });
+
+                dmgCreator.on('finish', () => {
+                  console.log(`\nPackage created in dist/Olden-${packageInfo.version}.dmg\n`);
+                  appPaths.forEach((p) => rimraf(p, () => {}));
+                });
+
+                dmgCreator.on('error', (err) => {
+                  console.log(`\n${err}\n`);
+                });
+              });
+          });
+        }
       }
-
-      dmgCreator = appdmg({
-        source: path.join(__dirname, '..', 'appdmg.json'),
-        target: path.join(__dirname, '..', 'dist', `Olden-${packageInfo.version}.dmg`)
-      });
-
-      dmgCreator.on('progress', (info) => {
-        if (info.type === 'step-begin') {
-          const line = `[ ${info.current}/${info.total} ] ${info.title}...`;
-
-          process.stdout.write(line + ' '.repeat(45 - line.length));
-        }
-
-        if (info.type === 'step-end') {
-          process.stdout.write(`[ ${info.status} ]\n`);
-        }
-      });
-
-      dmgCreator.on('finish', () => {
-        console.log(`\nPackage created in dist/Olden-${packageInfo.version}.dmg\n`);
-        appPaths.forEach((path) => rimraf(path, () => {}));
-      });
-
-      dmgCreator.on('error', (err) => {
-        console.log(`\n${err}\n`);
-      });
     }
   });
 } else {
